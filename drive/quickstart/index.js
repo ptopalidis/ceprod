115a31b19d6f89e102af40a5e0d4e00ce9adf0d9
TopalidisD <dimitrios.topalidis@e-food.gr>
2022-04-15 16:10:26 +0300

    15-04-2022

--- drive/quickstart/index.js
+++ drive/quickstart/index.js
@@ -13,11 +13,11 @@ const TOKEN_PATH = path.join(__dirname,"token.json")
 
 
 // Load client secrets from a local file.
-/*fs.readFile('credentials.json', (err, content) => {
+fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Drive API.
   authorize(JSON.parse(content), listFiles);
-});*/
+});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
